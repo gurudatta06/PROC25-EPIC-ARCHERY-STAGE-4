c7d310ad198f3006dd204068da07a897294357b5
gurudatta06 <88154474+gurudatta06@users.noreply.github.com>
2021-10-21 22:25:56 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -65,21 +65,21 @@ function draw() {
 
 function keyPressed() {
   if (keyCode === 32) {
-    var posX = playerArcher.body.position.x;
-    var posY = playerArcher.body.position.y;
-    var angle = playerArcher.body.angle;
-    var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+    var posX = playerArcher.body.position.x
+    var posY = playerArcher.body.position.y
+    var angle = playerArcher.body.angle
+    var arrow = new PlayerArrow(posX, posY, 100, 10, angle)
 
-    Matter.Body.setAngle(arrow.body, angle);
-    playerArrows.push(arrow);
+    Matter.Body.setAngle(arrow.body, angle)
+    playerArrows.push(arrow)
   }
 }
 
 function keyReleased() {
   if (keyCode === 32) {
     if (playerArrows.length) {
-      var angle = playerArcher.body.angle;
-      playerArrows[playerArrows.length - 1].shoot(angle);
+      var angle = playerArcher.body.angle
+      playerArrows[playerArrows.length - 1].shoot(angle)
     }
   }
 }
